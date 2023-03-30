f48cfbc759c8d175da8b09017b8c207d1a66230e
Oil <sudarat@1moby.com>
2023-03-30 18:42:25 +0700

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-console.log("process.env.NODE_ENV : ", process.env.NODE_ENV);
 const isProd = process.env.NODE_ENV === "production";
-console.log("isProd : ", isProd);
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? "/nummunoil" : "",
-  assetPrefix: isProd ? "/nummunoil/" : "./",
+  assetPrefix: isProd ? "/bill/" : "./",
   images: {
     unoptimized: true,
   },
