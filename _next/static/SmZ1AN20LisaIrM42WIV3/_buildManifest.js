80c41fea0b95f89132e99db17e24bef8982a0c57
nummunoil <nummunoil@users.noreply.github.com>
2023-03-30 11:05:36 +0000

    deploy: f7616daed6428fb6ad8dbc3f2a105498fe69b446

--- _next/static/SmZ1AN20LisaIrM42WIV3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-f6069a09857f080e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
