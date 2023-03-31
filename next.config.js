29a9ea0b3bb232179678a2ddcfd191a4f7da29ac
Oil <sudarat@1moby.com>
2023-03-31 23:58:21 +0700

    tailwind draft 1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+console.log("process.env.NODE_ENV : ", process.env.NODE_ENV);
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
